386f181143edb0512626bd822a462a5d14a0fc83
shawnboy777 <shawn.cutting@gmail.com>
2017-10-24 11:22:10 -0400

    Update viewmodel.js

--- src/js/viewmodel.js
+++ src/js/viewmodel.js
@@ -138,7 +138,7 @@ function initializeEditor(content, blockDefs, thumbPathConverter, galleryUrl) {
     debug: ko.observable(false),
     contentListeners: ko.observable(0),
     
-    logoPath: 'dist/img/mosaico32.png',
+    logoPath: 'dist/img/mc-email-logo.png',
     logoUrl: '.',
     logoAlt: 'mosaico'
   };
